fd173c3c548be507829f9b8e1799ad74654d1392
aluizio <aluizio@dcc.ufrj.br>
2015-08-19 15:51:54 -0300

    Changed the name of user in bookmark model.

--- bookmarker/models.py
+++ bookmarker/models.py
@@ -17,4 +17,4 @@ class Bookmark(models.Model):
     zoom = models.IntegerField(default=1)
     resourceType = models.CharField(choices=LIST, max_length=255, default='Communities')
     coordinates = models.CharField(max_length=1000, default='')
-    user = models.ForeignKey(User)
+    owner = models.ForeignKey(User)
