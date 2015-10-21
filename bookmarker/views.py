b928d255d0a0013512cded46aa45ab5d59769363
aluizio <aluizio@dcc.ufrj.br>
2015-10-21 11:23:57 -0200

    Added the authorization.

--- bookmarker/views.py
+++ bookmarker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from bookmarker.models import Bookmark
 from serializers import BookmarkSerializer
 
@@ -8,6 +10,16 @@ class BookmarksList(generics.ListCreateAPIView):
     queryset = Bookmark.objects.all()
     serializer_class = BookmarkSerializer
 
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    authentication_classes = (JSONWebTokenAuthentication,)
+
+    def post(self, request, *args, **kwargs):
+        request.data['owner'] = request.user.id
+        return self.create(request, *args, **kwargs)
+
 class BookmarksDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Bookmark.objects.all()
     serializer_class = BookmarkSerializer
+
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    authentication_classes = (JSONWebTokenAuthentication,)
