70c6183f2a8f21277ada38254b078ef4a5391edd
aluizio <aluizio@dcc.ufrj.br>
2016-09-13 16:54:45 -0300

    this app was deployed

--- markers/settings.py
+++ markers/settings.py
@@ -15,6 +15,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
